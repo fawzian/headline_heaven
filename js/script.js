751a01864647d6572c5c01a64896d130029c521f
FAWZIA NUR <FAWZIANUR@GMAIL.com>
2021-11-30 21:48:53 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// https://newsapi.org/v2/everything?q=keyword&apiKey=
+
+let newsData, userInput;
+
+const $headline = $('#headline');
+const $img = $('#img');
+const $description = $('#description');
+const $source = $('#source');
+const $more = $('#more');
+const $userInput = $('#input[type="text"]')
+
+
+$('form').on('submit', handleGetEvent)
+
+function handleGetEvent(event){
+    event.preventDefault();
+    userInput = $userInput.val();
+    console.log(userInput)
+    $.ajax({
+        url:'https://newsapi.org/v2/top-headlines?country='+ userInput +'&apiKey=cb96460cf0014cbb8070554b23a1459d'
+    }).then(
+        (data) => {
+        console.log(data);
+        newsData = data
+        $headline.text(newsData.articles[0].title);
+        $img.attr("src", newsData.articles[0].urlToImage);
+        $description.text(newsData.articles[0].description);
+        $source.text(newsData.articles[0].source.name);
+        $more.attr("href", newsData.articles[0].url)
+    },
+    (error) => {
+        console.log('bad request: ', error);
+    }
+    );
+}
+
+
